109226dbc02048fc0a92016b38b4ccc98b08ac42
Rhenriquez94 <91695458+Rhenriquez94@users.noreply.github.com>
2024-01-17 23:24:52 -0300

    Desafio 2- javascript

---
+++ Desafio-02/assets/js/Ejercicio_1.js
@@ -0,0 +1,11 @@
+function clickBorde(){
+    const imagen  = document.querySelector('#imgBorde')
+
+    if (imagen.style.borderColor === 'red' ){
+
+        imagen.style.borderColor ='transparent';
+    } else {
+        imagen.style.borderColor = 'red';
+    }
+}
+  
